d849fbc7816cfe5594d759afc7785d4bae210581
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-05-12 14:32:15 +0300

    Adding information about a function from a file processing.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from src.widget import get_mask_card_account, get_date_string
-
+from src.widget import get_date_string, get_mask_card_account
 
 print(get_mask_card_account("Счет 64686473678894779589"))
 print(get_mask_card_account("Visa Platinum 8990922113665229"))
 print(get_date_string("2018-07-11T02:26:18.671407"))
+
+from src.processing import get_new_list, get_sorted_list
+
+print(get_new_list([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+                   'EXECUTED'))
+
+print(get_sorted_list([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], reverse=True))
