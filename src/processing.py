3802bf72a1b45f638fe892e9bac175c3e968e841
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-05-12 14:29:42 +0300

    Adding a list sorting function

--- src/processing.py
+++ src/processing.py
@@ -12,8 +12,8 @@ def get_new_list(list_dict: list[dict[Any, str]], value='EXECUTED') -> list:
 
     return new_list
 
-print(get_new_list([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                   'EXECUTED'))
+def get_sorted_list(list_dict: list[dict[Any, str]], reverse=True) -> Any:
+    """Функция возвращает список словарей, отсортированных по дате операции"""
+
+
+    return sorted(list_dict, key=lambda my_dict: my_dict['date'], reverse=True)
