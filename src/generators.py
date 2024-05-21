d7e587e76ebd493e7d0d64c6241342499d112500
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-05-21 16:40:30 +0300

    Docstring added to functions

--- src/generators.py
+++ src/generators.py
@@ -2,17 +2,21 @@ from typing import Any, Generator, Iterator
 
 
 def filter_by_currency(transactions: list[dict], currency_code: str) -> Iterator[dict]:
+    """Функция, которая принимает список словарей с банковскими операциями и
+    возвращает итератор, выдающий по очереди операции в заданной валюте."""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency_code:
             yield transaction
 
 
 def transaction_descriptions(transactions: list[dict]) -> Generator[str, Any, None]:
+    """Функция создает генератор, который принимает список словарей и возвращает описание каждой операции по очереди."""
     for transaction in transactions:
         yield transaction["description"]
 
 
 def card_number_generator(start: int, stop: int) -> Generator[str, Any, None]:
+    """Функция создает генератор номеров банковских карт, который генерирует номера карт в определенном формате"""
     for num in range(start, stop + 1):
         number = "0" * (16 - len(str(num))) + str(num)
         card_number = f"{number[0:4]} {number[4:8]} {number[4:8]} {number[12:16]}"
