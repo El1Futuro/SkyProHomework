fd50d57bbed9b68e33ee57f61cf3bb54e1fb6709
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-05-26 14:29:05 +0300

    Creating decorators.py and test_decorators.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from src.decorators import log
 from src.widget import get_mask_card_account, get_date_string
 
 
 print(get_mask_card_account("Счет 64686473678894779589"))
 print(get_mask_card_account("Visa Platinum 8990922113665229"))
 print(get_date_string("2018-07-11T02:26:18.671407"))
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+
+my_function(1, 2)
+
+
+@log()
+def my_function_error(x, y):
+    return x / y
+
+
+my_function_error(2, 0)
