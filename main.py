f159b55af53bec787dd7966f7a68bcc97d4d42f0
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-06-15 00:07:41 +0300

    Added the ability to download data from a file Added the function of accessing an external API for currency conversion at the current exchange rate

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+from src.utils import get_list_transactions
 from src.widget import get_mask_card_account, get_date_string
+from src.external_api import get_amount_rub
+
+
 
 
 print(get_mask_card_account("Счет 64686473678894779589"))
 print(get_mask_card_account("Visa Platinum 8990922113665229"))
 print(get_date_string("2018-07-11T02:26:18.671407"))
+
+
+print()
+
+json_file_path = os.path.join("data", "operations.json")
+transactions = get_list_transactions(json_file_path)
+print(transactions)
+
+print()
+
+for transaction in transactions:
+    rub_amount = get_amount_rub(transaction)
+    print(f"Транзакция в RUB: {rub_amount}")
