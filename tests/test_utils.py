8124e395033c7024f781e21115716277aefe5dce
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-06-20 21:26:04 +0300

    Added functions for reading data from CSV and EXCEL files and tests. Updated README.md.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import unittest
-from unittest.mock import MagicMock, patch
+import pandas as pd
+from unittest.mock import MagicMock, patch, mock_open
 
-from src.utils import get_list_transactions
+from src.utils import get_list_transactions, get_transactions_from_csv, get_transactions_from_excel
 
 
 class TestGetListTransactions(unittest.TestCase):
@@ -21,3 +22,89 @@ class TestGetListTransactions(unittest.TestCase):
     def test_get_list_transactions_file_not_found(self) -> None:
         list_transactions = get_list_transactions("nonexistent_file.json")
         self.assertEqual(list_transactions, [])
+
+
+class TestGetTransactionsFromCsv(unittest.TestCase):
+
+    def test_get_transactions_from_csv_file_not_found(self) -> None:
+        csv_transactions = get_transactions_from_csv("nonexistent_file.csv")
+        self.assertEqual(csv_transactions, [])
+
+
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="id;state;date;amount;currency_name;currency_code;from;to;description\n"
+                  "4699552;EXECUTED;2022-03-23T08:29:37Z;23423;Peso;PHP;Discover 7269000803370165;"
+                  "American Express 1963030970727681;Перевод с карты на карту\n",
+    )
+    def test_get_transactions_from_csv_new(self, mock_file: MagicMock) -> None:
+        result = get_transactions_from_csv("test.csv")
+        self.assertEqual(
+            result,
+            [
+                {
+                    "id": "4699552",
+                    "state": "EXECUTED",
+                    "date": "2022-03-23T08:29:37Z",
+                    "amount": "23423",
+                    "currency_name": "Peso",
+                    "currency_code": "PHP",
+                    "description": "Перевод с карты на карту",
+                    "from": "Discover 7269000803370165",
+                    "to": "American Express 1963030970727681",
+                }
+            ],
+        )
+
+    # @patch("pd.read_excel.open")
+    # def test_get_transactions_from_excel_empty_file(self, mock_open: MagicMock) -> None:
+    #     mock_open.return_value.__enter__.return_value.pd.read_excel.return_value = ""
+    #     list_transactions = get_transactions_from_excel("test_file.json")
+    #     self.assertEqual(list_transactions, [])
+
+    @patch(
+            "builtins.open",
+            new_callable=mock_open,
+            read_data="")
+    def test_get_transactions_from_csv_new(self, mock_file: MagicMock) -> None:
+        result = get_transactions_from_csv("test.csv")
+        self.assertEqual(
+                result,
+                [])
+
+    data = {
+        "id": [4699552.0],
+        "state": ["EXECUTED"],
+        "date": ["2022-03-23T08:29:37Z"],
+        "amount": [23423.0],
+        "currency_name": ["Peso"],
+        "currency_code": ["PHP"],
+        "from": ["Discover 7269000803370165"],
+        "to": ["American Express 1963030970727681"],
+        "description": ["Перевод с карты на карту"],
+    }
+
+    df = pd.DataFrame(data)
+
+    df.to_excel("test.xlsx", index=False)
+
+    @patch("pandas.read_excel", return_value=df)
+    def test_get_transactions_from_excel(self, mock_read_excel: MagicMock) -> None:
+        result = get_transactions_from_excel("test.xlsx")
+        self.assertEqual(
+            result,
+            [
+                {
+                    "id": 4699552,
+                    "state": "EXECUTED",
+                    "date": "2022-03-23T08:29:37Z",
+                    "amount": 23423.0,
+                    "currency_name": "Peso",
+                    "currency_code": "PHP",
+                    "description": "Перевод с карты на карту",
+                    "from": "Discover 7269000803370165",
+                    "to": "American Express 1963030970727681",
+                }
+            ],
+        )
