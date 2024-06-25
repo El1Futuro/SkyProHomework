089908b33f5c958cc9f321d13b28588418faddee
Victoria_Zhuravleva <new.juravlik1334@gmail.com>
2024-06-25 12:12:30 +0300

    Changed description.py and test_processing.py

---
+++ src/description.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+from src.utils import get_list_transactions
+
+json_file_path = r"C:\Users\User\PycharmProjects\SecondHomework1\data\operations.json"
+list_transactions = get_list_transactions(json_file_path)
+
+
+def get_categories(list_transactions: list) -> list:
+    """Функция возвращает список всех категорий из поля 'description' в списке транзакций"""
+    categories = set()
+    for transaction in list_transactions:
+        if "description" in transaction:
+            categories.add(transaction["description"])
+    list_categories = list(categories)
+    return list_categories
+
+
+def count_operations_by_category(list_transactions: list, list_categories: list) -> dict:
+    """Функция выводит словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории⠀"""
+    counter = Counter()
+    for transaction in list_transactions:
+        if "description" in transaction:
+            if transaction["description"] in list_categories:
+                counter[transaction["description"]] += 1
+    print(dict(counter))
+
+
+get_categories(list_transactions)
+count_operations_by_category(list_transactions, get_categories(list_transactions))
---
+++ tests/test_processing.py
@@ -0,0 +1,63 @@
+import pytest
+from src.processing import get_sorted_list, filter_transactions_by_state
+
+
+def test_filter_transactions_by_state_empty() -> None:
+    assert filter_transactions_by_state([], "") == []
+
+
+def test_get_sorted_list_empty() -> None:
+    assert get_sorted_list([], True) == []
+
+
+@pytest.fixture
+def list_transactions() -> list:
+    return [
+        {"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
+    ]
+
+
+@pytest.fixture
+def value() -> str:
+    return "EXECUTED"
+
+
+def test_filter_transactions_by_state(list_transactions: list, value: str) -> None:
+    assert filter_transactions_by_state(list_transactions, value) != [
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    ]
+
+
+def test_get_sorted_list() -> None:
+    assert get_sorted_list(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        revers=True,
+    ) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+    assert get_sorted_list(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        revers=False,
+    ) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
